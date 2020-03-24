a31f2c7b8420255ff9c2d36808c6b66ca9932f22
Christophe Leray <christophe.leray1@gmail.com>
2020-03-24 17:16:10 +0100

    make both dice clickable at the same time

--- lib/main.dart
+++ lib/main.dart
@@ -25,35 +25,33 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 5;
+  void changeDiceface() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                  });
-                },
+      child: FlatButton(
+        onPressed: () {
+          changeDiceface();
+        },
+        child: Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
                 child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  });
-                },
+              Expanded(
+                flex: 1,
                 child: Image.asset('images/dice$rightDiceNumber.png'),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
